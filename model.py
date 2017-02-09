9dc1db27ae948f4da40ec47ccd22d12cd695c992
Shawn Tan <shawn@wtf.sg>
2017-02-09 01:06:15 -0500

    New method: 1. Downsample 2. Iterative fill 4. Upsample

--- model.py
+++ model.py
@@ -5,45 +5,108 @@ import deconv_ops
 import feedforward
 
 
+def build_conv_layer(P, name, input_size, output_size, rfield_size,
+                     activation=T.nnet.relu,
+                     weight_init=conv_ops.conv_weight_init):
+    P['W_conv_%s' % name] = weight_init(output_size,
+                                        input_size,
+                                        rfield_size)
+    P['b_conv_%s' % name] = np.zeros(output_size)
+    W = P['W_conv_%s' % name]
+    b = P['b_conv_%s' % name].dimshuffle('x', 0, 'x', 'x')
+
+    def convolve(X):
+        return activation(
+            T.nnet.conv2d(X, W, border_mode='half') + b)
+    return convolve
+
+
+def build_gated_conv_layer(P, name, input_size, output_size, rfield_size,
+                           activation=T.tanh):
+    def weight_init(output_size, input_size, rfield_size):
+        return feedforward.initial_weights(input_size * rfield_size**2,
+                                           output_size).reshape(output_size,
+                                                                input_size,
+                                                                rfield_size,
+                                                                rfield_size)
+
+    conv = build_conv_layer(P, name, input_size,
+                            2 * output_size, rfield_size,
+                            weight_init=weight_init,
+                            activation=lambda x: x)
+
+    def convolve(X):
+        lin = conv(X)
+        output = (T.nnet.sigmoid(lin[:, :output_size, :, :]) *
+                  activation(lin[:, output_size:, :, :]))
+        return output
+    return convolve
+
+
 def build(P):
-    conv_stack = conv_ops.build_stack(
-        P,
-        conv_filter_counts=[3, 32, 32, 64, 64],
-        conv_filter_sizes=[3, 3, 3, 3],
-        conv_pool_factors=[2, 2, 2, 2]
-    )
-
-    transconv_stack = deconv_ops.build_stack(
-        P, name="upsample",
-        conv_filter_counts=[64, 64, 32, 32, 3 * 256],
-        conv_filter_sizes=[5, 5, 5, 5],
-        conv_pool_factors=[2, 2, 2, 2],
-        activations=3 * [T.nnet.relu] + [lambda x: x]
-    )
-
-    mask = T.as_tensor_variable(
-        np.array([[1, 1, 1, 1],
-                  [1, 0, 0, 1],
-                  [1, 0, 0, 1],
-                  [1, 1, 1, 1]], dtype=np.int8)
+    FEATURE_STACK = 2
+    input_size = 3
+    downsample = [None] * FEATURE_STACK
+    for i in xrange(FEATURE_STACK):
+        downsample[i] = conv_ops.build_conv_and_pool(
+            P, name="downsample_%d" % i,
+            input_size=input_size,
+            output_size=32,
+            filter_size=5,
+            pool_factor=2,
+            activation=T.nnet.relu
         )
+        input_size = 32
 
-    transform_1 = feedforward.build_combine_transform(
-        P, "fully_connected_1",
-        input_sizes=[16 * 64],
-        output_size=4 * 64,
-        initial_weights=feedforward.relu_init,
-        activation=T.nnet.relu,
-        batch_norm=False)
+    inpaint_iterator = build_gated_conv_layer(
+        P, name="inpaint_iterator",
+        input_size=32,
+        output_size=32,
+        rfield_size=5,
+    )
+
+    output_transform = build_conv_layer(
+        P, name="output",
+        input_size=32,
+        output_size=3 * 256,
+        rfield_size=1
+    )
+
+    upsample = [None] * FEATURE_STACK
+    for i in xrange(FEATURE_STACK):
+        upsample[i] = deconv_ops.build_upsample_and_conv(
+            P, name="upsample_%d" % i,
+            input_size=input_size,
+            output_size=32,
+            filter_size=5,
+            pool_factor=2,
+            activation=T.nnet.relu
+        )
+        input_size = 32
 
     def inpaint(X):
         batch_size, channels, img_size_1, img_size_2 = X.shape
-        X = T.set_subtensor(X[:, :, 16:48, 16:48], 0)
-        in_feature_maps = T.switch(mask[None, None, :, :],
-                                   conv_stack(X), 0)
-        out_feature_maps = transform_1([in_feature_maps.flatten(2)])
-        out_feature_maps = out_feature_maps.reshape((batch_size, 64, 2, 2))
-        output = transconv_stack(out_feature_maps)
+        down_X = T.set_subtensor(X[:, :, 16:48, 16:48], 0)
+        down_X = downsample[0](down_X)
+        down_X = downsample[1](down_X)
+        # batch_size, 32, 16, 16
+
+        fill_X = T.set_subtensor(down_X[:, :, 4:12, 4:12], 0)
+        fill_X = inpaint_iterator(fill_X)
+        fill_X = T.set_subtensor(down_X[:, :, 5:11, 5:11], 0)
+        fill_X = inpaint_iterator(fill_X)
+        fill_X = T.set_subtensor(down_X[:, :, 6:10, 6:12], 0)
+        fill_X = inpaint_iterator(fill_X)
+        fill_X = T.set_subtensor(down_X[:, :, 7:9, 7:9], 0)
+        fill_X = inpaint_iterator(fill_X)
+
+        # batch_size, 32, 8, 8
+        up_Y = fill_X[:, :, 4:12, 4:12]
+        up_Y = upsample[0](up_Y)
+        up_Y = upsample[1](up_Y)
+
+        output = output_transform(up_Y)
+
         return output
 
     return inpaint
